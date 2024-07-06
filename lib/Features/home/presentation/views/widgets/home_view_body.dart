1f1b88df6bb5446b816944c07563b827ae433dcf
KhaledHAnter <khaldntr11@gmail.com>
2024-07-06 06:05:49 +0300

    Custom Scroll for home view

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/Core/utils/styles.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -11,24 +11,29 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        Gap(40),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Text(
-            "Best Seller",
-            style: Styles.textStyle18,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CustomAppBar(),
+              FeaturedBooksListView(),
+              Gap(40),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  "Best Seller",
+                  style: Styles.textStyle18,
+                ),
+              ),
+              Gap(20),
+            ],
           ),
         ),
-        Gap(20),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: BestSellerListViewItem(),
-        ),
+        SliverFillRemaining(
+          child: BestSellerListView(),
+        )
       ],
     );
   }
