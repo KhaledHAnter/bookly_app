06de91c560d4af06d6b4f9489da0e154a675c09a
KhaledHAnter <khaldntr11@gmail.com>
2024-07-11 20:54:11 +0300

    add title & subtitle to book details veiw

--- lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,67 +1,74 @@
+import 'package:bookly_app/Core/utils/app_router.dart';
 import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 2 / 3,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  image: AssetImage(AssetsData.testImage2),
-                  fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 2 / 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.testImage2),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
-          ),
-          const Gap(30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    "Harry Potter and the Goblet of Fire",
-                    style:
-                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                const Gap(3),
-                const Text(
-                  "J.K. Rowling",
-                  style: Styles.textStyle14,
-                ),
-                const Gap(3),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "19.99 €",
+            const Gap(30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      "Harry Potter and the Goblet of Fire",
                       style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold),
+                          .copyWith(fontFamily: kGtSectraFine),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    const Spacer(),
-                    const BookRating(),
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+                  ),
+                  const Gap(3),
+                  const Text(
+                    "J.K. Rowling",
+                    style: Styles.textStyle14,
+                  ),
+                  const Gap(3),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "19.99 €",
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,40 @@
+import 'package:bookly_app/Core/utils/styles.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: Column(
+        children: <Widget>[
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.20),
+            child: const FeaturedListViewItem(),
+          ),
+          const Gap(43),
+          const Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          const Gap(6),
+          Opacity(
+            opacity: 0.8,
+            child: Text("Rudyard Kipling",
+                style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w500)),
+          ),
+        ],
+      ),
+    );
   }
 }
