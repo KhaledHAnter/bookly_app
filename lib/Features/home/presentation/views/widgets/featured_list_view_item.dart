cc3480512d4e13bf1bf2e00eac92c5eedf2349f7
KhaledHAnter <khaldntr11@gmail.com>
2024-07-18 16:32:31 +0300

    cached network image

--- lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/Core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class FeaturedListViewItem extends StatelessWidget {
   const FeaturedListViewItem({super.key, required this.imageUrl});
@@ -8,17 +10,27 @@ class FeaturedListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2 / 3,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          image: DecorationImage(
-            image: NetworkImage(
-              imageUrl,
-            ),
-            fit: BoxFit.fill,
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: AspectRatio(
+        aspectRatio: 2 / 3,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) {
+            return const Center(
+              child: Icon(
+                Icons.error,
+                color: Colors.red,
+              ),
+            );
+          },
+          placeholder: (context, url) {
+            return Center(
+              child: LoadingAnimationWidget.discreteCircle(
+                  color: Colors.white, size: 50),
+            );
+          },
         ),
       ),
     );
